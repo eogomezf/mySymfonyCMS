aafa6114b8121f6b20b6e5db91520c16ebe3532a
Elvis Gomez <eogomezf@gmail.com>
2019-04-01 12:52:26 -0600

    Rutas method apis

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -18,4 +21,49 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/producto", name="productos_list")
+     * @Method({"GET"})
+     */
+    public function listProducto(Request $request)
+    {
+        return new Response(
+            '<html><body>'.'Nro de Productos: '.$request->get('nro').'</body></html>'
+        );
+    }
+
+    /**
+     * @Route("/producto/new", name="cms_producto_new")
+     * @Method({"POST"})
+     */
+    public function newProducto(Request $request)
+    {
+        return new Response(
+            json_encode($request->request->all())
+        );
+    }
+
+    /**
+     * @Route("/producto/{id}/edit", name="cms_producto_edit")
+     * @Method({"PUT"})
+     */
+    public function editProducto($id)
+    {
+        return new Response(
+            'Editar Producto ID: '.$id
+        );
+    }
+
+    /**
+     * @Route("/producto/{id}", name="cms_producto_delete")
+     * @Method({"DELETE"})
+     */
+    public function deteleProducto($id)
+    {
+        return new Response(
+            'Borrar Producto ID: '.$id
+        );
+    }
+
 }
--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -4,14 +4,19 @@ namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/list/{page}", name="blog_list", requirements={"pages": "\d+"})
+     * @Method({"GET"})
      */
-    public function indexAction()
+    public function indexAction($page=1)
     {
-        return $this->render('BlogBundle:Default:index.html.twig');
+        return $this->render('BlogBundle:Default:index.html.twig',
+        array(
+            'page' => $page
+        ));
     }
 }
